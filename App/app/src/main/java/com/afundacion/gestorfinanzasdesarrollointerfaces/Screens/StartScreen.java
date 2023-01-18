a575404127406b408f87d790170dc00d749f452d
Diego Fernández Sierra <dfernandezs@fpcoruna.afundacion.org>
2023-01-18 10:13:35 +0100

    Pantalla de agregar transacciones hecha

---
+++ App/app/src/main/java/com/afundacion/gestorfinanzasdesarrollointerfaces/Screens/StartScreen.java
@@ -0,0 +1,16 @@
+package com.afundacion.gestorfinanzasdesarrollointerfaces.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.afundacion.gestorfinanzasdesarrollointerfaces.R;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
